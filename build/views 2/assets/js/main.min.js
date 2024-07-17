3579428a6ae40adebcb9f9f100d5e21bec75389c
coder371 <abdalstar371371@gmail.com>
2024-07-17 12:44:46 +0300

    sve

--- build/views 2/assets/js/main.min.js
+++
@@ -1,7 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
--- src/routes/product.router/index.js
+++ src/routes/product.router/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const { productController } = require('../../controllers');
 
 /* GET home page. */
-router.get('' , productController);
+router.get('/:slug' , productController);
 
 module.exports = router;
